122c43dcd6d9df8c9d0d41bb913d189a2c717148
Luca Valnegri <l.valnegri@datamaps.co.uk>
2020-08-06 13:23:53 +0100

    added some feedback text when postcode is invalid

--- ui.R
+++ ui.R
@@ -3,42 +3,39 @@
 #################################
 
 fluidPage(
-    
+
     titlePanel('UK Eatout'),
-    
+
     sidebarLayout(
-        
+
         sidebarPanel(
-            
+
             pickerInput('cbo_geo', 'GEOGRAPHY:', lcn.tpe),
-            
+
             uiOutput('ui_1st'),
-                     
+
             uiOutput('ui_2nd'),
-            
+
             tags$br(),
-            
-            column(12, actionButton('btn_go', 'FIND RESTAURANTS'), align = "center", style = "margin-bottom: 10px;margin-top: -10px;"), 
-      #            actionButton('btn_go', 'FIND RESTAURANTS'),
-            tags$br(),
-            
-            
+            column(12, actionButton('btn_go', 'FIND RESTAURANTS'), align = "center", style = "margin-bottom:30px;margin-top:-10px;"), 
+            HTML('<p>Source: <a href="https://github.com/hmrc/eat-out-to-help-out-establishments" target="_blank">HMRC</a></p>'),
+
             width = 3
-            
+
         ),
-        
+
         mainPanel(
-                  
+
             tabsetPanel(
-                                               
-                tabPanel('Map', withSpinner(leafletOutput('out_map', height = '600px') ) ),
-            
-                tabPanel('Table', withSpinner(DTOutput('out_tbl') ) )
-            
+
+                tabPanel('Map', tags$br(), withSpinner(leafletOutput('out_map', height = '600px') ) ),
+
+                tabPanel('Table', tags$br(), textOutput('out_txt'), withSpinner(DTOutput('out_tbl') ) )
+
             )
-            
+
         )
-        
+
     )
-    
+
 )
