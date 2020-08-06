e66470c893f60f10cc8d2a0e4c16172788aa3e05
Luca Valnegri <l.valnegri@datamaps.co.uk>
2020-08-06 13:23:11 +0100

    - changed default icons with self-built
    - add layers to basemap
    
    - added constants for leaflet tiles and labels options

--- global.R
+++ global.R
@@ -10,12 +10,40 @@ options(bitmapType = 'cairo', shiny.usecairo = TRUE)
 
 dts <- read_fst(file.path(app_path, 'uk_eatout', 'dataset'), as.data.table = TRUE)
 pc <- read_fst(file.path(geouk_path, 'postcodes'), columns = c('postcode', 'x_lon', 'y_lat'), as.data.table = TRUE)
-lcn <- read_fst( file.path(geouk_path, 'locations'), columns = c('type', 'location_id', 'name', 'x_lon', 'y_lat'), as.data.table = TRUE)
 
 lcn.tpe <- c(
     'Postcode [Bounding Box]' = 'PCU', 'Postcode Sector ' = 'PCS', 'Postal Town' = 'PCT', 'Postcode District' = 'PCD',
     'Ward' = 'WARD', 'Parish' = 'PAR'
 )
+
+lbl.options <- labelOptions(
+    nohide = TRUE,
+    textsize = '12px',
+    direction = 'right', 
+    sticky = FALSE, 
+    opacity = 0.8,
+    offset = c(10, -10), 
+    style = list(
+        'color' = 'black',
+        'border-color' = 'rgba(0,0,0,0.5)',
+        'font-family' = 'verdana',
+        'font-style' = 'normal',
+        'font-size' = '14px',
+        'font-weight' = 'normal', 
+        'padding' = '2px 6px',
+        'box-shadow' = '3px 3px rgba(0,0,0,0.25)'
+    )
+)
+
+addLegendFixedCustom <- function(map, colors, labels, sizes = 20, opacity = 0.5, radius = 50, ...){
+    colorAdditions <- paste0(colors, ';margin-top:4px;margin-bottom:4px;border-radius:', radius, '%;width:', sizes, 'px;height:', sizes, 'px')
+    labelAdditions <- paste0(
+        '<div style=display:inline-block;height:', sizes, 'px;margin-top:4px;margin-bottom:4px;line-height:', sizes, 'px;>',
+        labels, 
+        '</div>'
+    )
+  return(addLegend(map, colors = colorAdditions, labels = labelAdditions, opacity = opacity, ...))
+}
 
 mp <- leaflet(options = leafletOptions(minZoom = 6)) %>% 
         setView(lat = 54.003419, lng = -2.547973, zoom = 6) %>% 
@@ -23,8 +51,29 @@ mp <- leaflet(options = leafletOptions(minZoom = 6)) %>%
         addTiles(options = tileOptions(useCache = TRUE, crossOrigin = TRUE)) %>% 
         addSearchOSM() %>%
         addResetMapButton() %>%
-        addFullscreenControl() 
-# for(idx in 1:length(tiles.lst)) mp <- mp %>% addProviderTiles(providers[[tiles.lst[idx]]], group = names(tiles.lst)[idx])
+        addFullscreenControl()
+tiles.lst <- c(
+    'OSM Mapnik' = 'OpenStreetMap.Mapnik', 
+    'OSM B&W' = 'OpenStreetMap.BlackAndWhite', 
+    'Stamen Toner' = 'Stamen.Toner', 
+    'Stamen Toner Lite' = 'Stamen.TonerLite'
+)
+for(idx in 1:length(tiles.lst)) 
+    mp <- mp %>% addProviderTiles(providers[[tiles.lst[idx]]], group = names(tiles.lst)[idx])
+mp <- mp %>% 
+        addLayersControl( baseGroups = names(tiles.lst), options = layersControlOptions(collapsed = FALSE) ) %>% 
+    	addLegendFixedCustom(
+            colors = c('orange', 'lightblue'),
+            labels = c('Big Chains', 'Other Shops'),
+    		opacity = 1,
+            title = '',
+            position = 'bottomright',
+    	)
+
+markers <- iconList(
+    red = makeIcon(file.path(pub_path, 'images', 'icons', 'leaflet', 'markers', 'sm-orange.png'), iconWidth = 24, iconHeight =32),
+    blue = makeIcon(file.path(pub_path, 'images', 'icons', 'leaflet', 'markers', 'sm-lightblue.png'), iconWidth = 24, iconHeight =32)
+)
 
 bounding_box <- function(lat, lon, dist, in.miles = TRUE) {
 
@@ -44,8 +93,7 @@ bounding_box <- function(lat, lon, dist, in.miles = TRUE) {
     lonr <- deg2rad(lon)
     dlon <- asin(sin(r)/cos(latr))
 
-    m <- matrix(c(lon_range(lonr = lonr, dlon = dlon), 
-        lat_range(latr=latr, r=r)), nrow=2, byrow = TRUE)
+    m <- matrix(c(lon_range(lonr = lonr, dlon = dlon), lat_range(latr=latr, r=r)), nrow=2, byrow = TRUE)
 
     dimnames(m) <- list(c("lng", "lat"), c("min", "max"))
     m
